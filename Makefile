fc322ea65102ef350c700274715a70bdf2196801
Ihor K <lov3catch@gmail.com>
2021-04-11 15:27:27 +0300

    - fix errors

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 type-check:
 	php vendor/bin/psalm -c psalm.xml
+
+run-tests:
+	php vendor/bin/phpunit tests/*
---
+++ tests/DummyTestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+class DummyTestCase extends \PHPUnit\Framework\TestCase
+{
+    public function testFoo(): void
+    {
+       self::assertEquals(1, 2);
+    }
+}
