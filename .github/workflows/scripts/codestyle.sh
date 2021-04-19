2eba6fea28534a424e17d928c52f6f4f514fdb8c
Ihor K <lov3catch@gmail.com>
2021-04-19 19:44:03 +0300

    - check codestyle

--- .github/workflows/scripts/codestyle.sh
+++ .github/workflows/scripts/codestyle.sh
@@ -6,5 +6,6 @@ target='develop'
 changed_files=$(git diff --name-only --line-prefix=`git rev-parse --show-toplevel`/ master..develop)
 
 echo $changed_files
+echo "${GITHUB_WORKSPACE}/.github/workflows/scripts/codestyle.sh"
 
-../vendor/squizlabs/php_codesniffer/bin/phpcs $changed_files
+../../../vendor/squizlabs/php_codesniffer/bin/phpcs $changed_files
