029722633973477f3a10bb8fbb9fb3a12721ce4d
Ihor K <lov3catch@gmail.com>
2021-04-20 14:11:55 +0300

    - check codestyle

--- scripts/codestyle_check.sh
+++ scripts/codestyle_check.sh
@@ -3,6 +3,12 @@
 source=$1
 target=$2
 
+if test -z "$source" || test -z "$target"
+then
+      echo "Need to specify target and source destinations to get changed files and calculate git diff"
+      exit 1
+fi
+
 changed_files=$(git diff --name-only --line-prefix=`git rev-parse --show-toplevel`/ $source..$target)
 
 ./vendor/squizlabs/php_codesniffer/bin/phpcs --standard=PSR12 $changed_files
